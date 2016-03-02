6d3e1af6eb2b0b8134c54fbeafa1905548040156
cloudcello <ef256aff@opayq.com>
2016-03-02 22:05:28 +0300

    Adds a Laddered Shift Counter for the Graph; +dist
    Each intersection with each axis now has a counter, so every line is
    properly padded. Adds dist parameter 0-no distance b/n groups -0.9999 -
    almost complete overlap, 1 - dist. of 1 group of lines. Adds
    description.

--- R/visualization.R
+++ R/visualization.R
@@ -1,3 +1,18 @@
+#------------------------------------------------------------------------------#
+# Description: a visualization function that uses parallel coordinates 
+#              to plot argument options as levels along each axis. 
+#              The function plots combinations that are associated with 
+#              successful ('PASS') tests first with failing tests afterwards
+#              so failing test settings (i.e. their graph representation) 
+#              are never overlapped by successful test settings.
+#              It is possible to plot either 'PASS' or 'FAIL' settings alone
+#              (see the description of the plot.test() function).
+#              
+# Author: cloudcell
+# Date: 2016-03
+# License: GPL-3
+#------------------------------------------------------------------------------#
+
 
 # References:
 #
@@ -13,7 +28,7 @@
 # TODO: use this for shuffling columns & other neat stuff !
 #       https://syntagmatic.github.io/parallel-coordinates/
 
-# generates analytics for plotting
+# generates analytics for plotting (internal function)
 generate.analytics <- function(env=cont.env, verbose=FALSE, DEBUG=FALSE)
 {
     # use this name to debug the function running its code outside the function
@@ -47,7 +62,6 @@ generate.analytics <- function(env=cont.env, verbose=FALSE, DEBUG=FALSE)
     if(verbose) message("Transformed test results saved in the test environment")
 }
 
-
 # a quote from https://en.wikipedia.org/wiki/Parallel_coordinates
 # "
 # When used for statistical data visualisation there are three important
@@ -57,14 +71,13 @@ generate.analytics <- function(env=cont.env, verbose=FALSE, DEBUG=FALSE)
 # analysis many reorderings will need to be tried. Some authors have come up
 # with ordering heuristics which may create illuminating orderings.
 # "
-#
 
 
-# uses parallelplot with added 'jitter' to diplay better view of combinations of
-# argument options that lead to failure of a function
-# TODO: allow to save as a *.PDF file for further analysis
-# (pdf allows for increased magnification)
-# 
+#' uses parallelplot with added spacing to diplay better view of 
+#' combinations of argument options leading to either PASS/FAIL exit status
+#' TODO: allow to save as a *.PDF file for further analysis
+#' (pdf allows for increased magnification)
+#' 
 #' @param env work environment, set only if default environment is not used
 #' @param pass determines whether the chart includes the full test set data.
 #'             I.e. whether all of the 'PASS' tests are included (default is 
@@ -74,19 +87,32 @@ generate.analytics <- function(env=cont.env, verbose=FALSE, DEBUG=FALSE)
 #'             I.e. whether all of the 'PASS' tests are included (default is 
 #'             TRUE). If set to FALSE, all the PASS data will not be drawn, 
 #'             which sometimes might speed up the drawing process.)
+#' @param dist the minimum distance between two adjacent lines from different
+#'             'levels' within the same 'category' at a point of intersection 
+#'             with the category ('argument') axis. The default of 1.0
+#'             makes a gap between any two adjacent groups of lines 
+#'             sufficient for a maximum possible number of lines to pass through 
+#'             without overlapping adjacent lines at the point of intersection
+#'             with the axis. Distance of 0 makes no gap (with only a 
+#'             regular distance separating any two adjacent lines at a point
+#'             of intersection with the vertical axis). The value of dist must 
+#'             be greater than -1.
 #' @param verbose provides additional text output during processing
 #' @param DEBUG enters the debug mode on function entry
 #' 
 #' @author cloudcello
 #' 
 #' @export             
-plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE, 
+plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE, dist=1.0,
                        verbose=FALSE, DEBUG=FALSE)
 {
-
+    browser(expr=DEBUG)
+    
     cont.env=env
     r <- cont.env$arg_register
 
+    if(dist<=-1) stop("The value of dist must be greater than -1")
+    
     # prepare data for plotting, if data does not yet exist
     # if(is.null(cont.env$failure_map)) {
     # message("Data for plotting has not yet been generated. Generating data.")
@@ -112,9 +138,31 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
     success_map <- cont.env$success_map
     nr_s <- nrow(success_map)
     
+    nr_all <- nr_s + nr_f + 2 # 2 == rn_boundary
+    
     # col. names
     xlab.str <- names(cont.env$arg_register)
     
+    #--------------------------------------------------------------------------#
+    # preparing a laddered shift counter for the graph
+    graph_shift <- list()
+    max_levels <- 0
+    for (i in 1:length(r)) {
+        level_qty <- length(r[[i]])
+        if(verbose) message("level_qty= ",level_qty)
+        if(max_levels<level_qty) { max_levels <- level_qty }
+        
+        graph_shift[[i]] <- vector(mode = "numeric", length = level_qty)
+        if(verbose) message("graph_shift= ", graph_shift)
+    }
+    axes_names <- names(r)
+    graph_shift <- setNames(graph_shift, axes_names)
+    
+    # duplicate the structure: the duplicate will be used to calculate the
+    # size of a shift based on the total number of 'hits' in each 'node'
+    graph_shift_base <- graph_shift
+    
+    # browser()
     #--------------------------------------------------------------------------#
     # preparing data related to exit status 'FAIL'
 
@@ -122,23 +170,39 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
     failure_map_parplot <- failure_map
     if(verbose) message(str(failure_map))
 
+    if(0) {
+        # count the number of times each argument option gets 'hit'
+        i=0; while (i<nr_f) { 
+            i=i+1;
+            for(j in 1:ncol(failure_map_parplot)) {  # TODO: take a table with ALL results (better style)
+                # take a "clean level" as a coordinate 
+                level <- failure_map_parplot[i,j]
+                graph_shift_base[[j]][level] <- graph_shift_base[[j]][level] + 1
+            }
+        }
+    }
+    
     i=0; while (i<nr_f) { i=i+1;
-    # for(i in seq_along(nr)){
-        # for(j in 1:ncol(failure_map_parplot)) {
-            # opt_nbr_max <- length(r[[j]])
-            # cat(opt_nbr_max)
-            # The max value one can add and still avoid wrong association among
-            # adjacent argument option numbers is 1: the only necessary condition is
-            # ( jitter < 1 ) as factors/option numbers are separated by a distance 1
-
-            # jitter <- (i / nr) / (9 - opt_nbr_max) #5 # also shrink by 3
-            jitter <- ((i-1) / (nr_s+nr_f)) / 3 # also shrink by 3 ( beginning with 0!)
-
-            # The max shift must NOT depend on the max number of options per
-            # variable as the scaling is done by the plot function itself !
-            # failure_map_parplot[i,j] <- failure_map_parplot[i,j] + jitter
-            failure_map_parplot[i,] <- failure_map_parplot[i,] + jitter
-        # }
+        for(j in 1:ncol(failure_map_parplot)) {  # TODO: take a table with ALL results (better style)
+            
+            # take a "clean level" as a coordinate before adjusting it
+            # to be come an "adjusted level" to be put on the plot 
+            level <- failure_map_parplot[i,j]
+            
+            failure_map_parplot[i,j] <- failure_map_parplot[i,j] + graph_shift[[j]][level]
+            
+            # gap <- (1 / (nr_s+nr_f)) / 3 # also shrink by 3 ( beginning with 0!)
+            
+            # gap <- ( 1 / graph_shift_base[[j]][level] ) / 3
+            # gap <- ( 1 / max_levels ) / 3
+            gap <- ( 1 / max_levels ) / (1 + dist) # TODO get it out of the loop!
+            
+            # message("col j = ", j, " row i= ", i, " total gap = ", graph_shift[[j]][level])
+            
+            
+            # plotting starts from with no additional gap at all
+            graph_shift[[j]][level] <- graph_shift[[j]][level] + gap
+        }
     }
     parplot_boundary_f <- failure_map_parplot[i,]
     
@@ -156,26 +220,39 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
     success_map_parplot <- success_map
     if(verbose) message(str(success_map))
 
+    if(0) {
+        # count the number of times each argument option gets 'hit'
+        i=0; while (i<nr_s) { 
+            i=i+1;
+            for(j in 1:ncol(success_map_parplot)) {  # TODO: take a table with ALL results (better style)
+                # take a "clean level" as a coordinate 
+                level <- success_map_parplot[i,j]
+                graph_shift_base[[j]][level] <- graph_shift_base[[j]][level] + 1
+            }
+        }
+    }
+    
     i=0; while (i<nr_s) { i=i+1;
-    # for(i in seq_along(nr)){
-        # for(j in 1:ncol(failure_map_parplot)) {
-        # opt_nbr_max <- length(r[[j]])
-        # cat(opt_nbr_max)
-        # The max value one can add and still avoid wrong association among
-        # adjacent argument option numbers is 1: the only necessary condition is
-        # ( jitter < 1 ) as factors/option numbers are separated by a distance 1
+        for(j in 1:ncol(success_map_parplot)) { # TODO: take a table with ALL results (better style)
+            
+            # take a "clean level" as a coordinate before adjusting it
+            # to be come an "adjusted level" to be put on the plot 
+            level <- success_map_parplot[i,j]
+            
+            success_map_parplot[i,j] <- success_map_parplot[i,j] + graph_shift[[j]][level] 
 
-        # jitter <- (i / nr) / (9 - opt_nbr_max) #5 # also shrink by 3
-        # jitter <- (nr_s - (i - 1) / nr_s) / 4 # also shrink by 3 
-        jitter <- ((nr_f+i - 1 ) / (nr_s+nr_f)) / 3 # also shrink by 3
-        # shifted up a bit 
-
-        # The max shift must NOT depend on the max number of options per
-        # variable as the scaling is done by the plot function itself !
-        # failure_map_parplot[i,j] <- failure_map_parplot[i,j] + jitter
-        # success_map_parplot[i,] <- success_map_parplot[i,] + jitter
-        success_map_parplot[i,] <- success_map_parplot[i,] + jitter
-        # }
+            # gap <- (1 / (nr_s+nr_f)) / 3 # also shrink by 3 ( beginning with 0!)
+            
+            # gap <- ( 1 / graph_shift_base[[j]][level] ) / 3
+            # gap <- ( 1 / max_levels ) / 3
+            gap <- ( 1 / max_levels ) / (1 + dist) # TODO get it out of the loop!
+            
+            # plotting starts from with no additional gap at all
+            graph_shift[[j]][level] <- graph_shift[[j]][level] + gap
+            
+            # message("col j = ", j, " row i= ", i, " total gap = ", graph_shift[[j]][level])
+            
+        }
     }
     parplot_boundary_p <- success_map_parplot[i,] # 'p' == 'pass'
     
@@ -186,99 +263,28 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
     colnames(success_map_parplot) <- xlab.str
     #--------------------------------------------------------------------------#
 
-    
-    # setnames(data, old=c() new=c()) in library(data.table)
-    
-
-    # TODO: print everything on the same chart:
-    # reference: http://stackoverflow.com/questions/6853204/plotting-multiple-curves-same-graph-and-same-scale
-    # par(new = TRUE)
-
-    ## plotting several classes in various colors
-    ## Ref: http://www.public.iastate.edu/~maitra/stat501/Rcode/parallelplot.R
-    ##      see at the bottom !
-
-    ## also, see argument 'groups'
-    #  groups A variable or expression to be
-    #  evaluated in data, expected to act as a grouping variable within each
-    #  panel, typically used to distinguish different groups by varying graphical
-    #  parameters like color and line type. Formally, if groups is specified, then
-    #  groups along with subscripts is passed to the panel function,
-    #-> good example for the 'group' parameter
-    #   http://www.r-bloggers.com/conditioning-and-grouping-with-lattice-graphics/
-    #
-    #-> how to use groups:
-    #    http://science.nature.nps.gov/im/datamgmt/statistics/r/graphics/lattice.cfm
-    #    read that page, which includes the following, among other things:
-    #    #---------------------------------------------------------------------#
-    #    | y ~ x | b, groups=a    separate plots for each value of b of y ~ x, |
-    #    | with dfferent colors or symbols for different values of a           |
-    #    #---------------------------------------------------------------------#
-    #
-# if(0) {
-#     if(verbose) message("drawing 'success map'")
-#     # draw 'success' parallelplot to properly set the scale for all axes
-#     if(1) {
-#     # par(new = TRUE)
-#         chart_success <-
-#             parallelplot(success_map_parplot, horizontal.axis=FALSE, col="grey",
-#                          main="Argument-Option Combinations Correlated with Test Result 'PASS'", #(nr_s)
-#                          drop.unused.levels=FALSE)
-#         # print(chart_success)
-#         print(chart_success, more=TRUE)
-#     }
-# 
-#     if(verbose) message("drawing 'failure map'")
-#     # TODO: add arg. names as labels to the graph
-#     chart <- parallelplot(failure_map_parplot, horizontal.axis=FALSE, col=rainbow(nr_f),
-#                  main="Argument-Option Combinations Correlated with Test Result 'FAIL'",
-#                  drop.unused.levels=FALSE)
-#                  # ,                 scales=list(cex=1))
-#                  # main="Argument-Option Combinations Correlated with Function Failure")
-#     # par(new = TRUE)
-#     # plot(chart)
-#     print(chart)#, more=TRUE) # finalize printing
-#     print(chart_success)#, more=TRUE)
-# }
-    
     if(verbose) message("drawing 'full test map'")
     
-    browser(expr=DEBUG)
+    # browser()
+    
     #--------------------------------------------------------------------------#
     # determine the two 'line coordinates' for the boundary (top & bottom):
 
-    # TODO: factor out this to an external function
-    parplot_boundary_p
-    parplot_boundary_f
-    
     # min/max boundaries based on actual data
-    
     tmp <- rbind(parplot_boundary_p,parplot_boundary_f)
     parplot_boundary_max <- apply(X = tmp, MARGIN = 2, FUN = max) # could be taken from arg_register
-    parplot_boundary_min <- apply(X = tmp, MARGIN = 2, FUN = min) # should usually be 1 for all options
-    
-    # str(parplot_boundary_max)
     parplot_boundary_max <- t(parplot_boundary_max)
-    parplot_boundary_min <- t(parplot_boundary_min)
-    
-    # parplot_boundary_max <- t(as.matrix(parplot_boundary_max))
     parplot_boundary_max <- as.data.frame(parplot_boundary_max, row.names = "0")
-    parplot_boundary_min <- as.data.frame(parplot_boundary_min, row.names = "0")
-    
     colnames(parplot_boundary_max) <- xlab.str
-    colnames(parplot_boundary_min) <- xlab.str
     
+    # simply copy to use as a ready template
+    parplot_boundary_min <- parplot_boundary_max 
+    # the minimum group number should always == 1, for all args !
+    parplot_boundary_min[1,] <- 1
     
-    # min/max boundaries based on actual data (not implemented for MAX yet)
-    if(0) { 
-        # just to have a template
-        parplot_boundary_min <- parplot_boundary_max 
-        
-        # the minimum group number is always 1 in this case
-        parplot_boundary_min[1,] <- 1
-    }
-        
     parplot_boundaries <- rbind(parplot_boundary_max, parplot_boundary_min)
+    
+    # add 2 col's - status(pass/fail/'TECH'-nical) & color:
     
     # color_technical <- as.character("#FF0000FF") # TODO: change to all 'F' - this is for debugging only
     color_technical <- as.character("#11111105") # TODO: change to all 'F' - this is for debugging only
@@ -286,7 +292,6 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
     tech <- cbind(parplot_boundaries, fld="TECH", 
                  color=color_technical, stringsAsFactors=FALSE) # grey
         
-    ## add 2 col's - status(pass/fail/'TECH'-nical) & color
     
     #--------------------------------------------------------------------------#
     # assigning technical fields for pass & fail data
@@ -344,7 +349,7 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
         stop("No test results are available for graphing.")
     }
     
-    nr_all <- nr_s + nr_f + 2 # 2 == rn_boundary
+    # nr_all <- nr_s + nr_f + 2 # 2 == rn_boundary
         
     if(verbose) message( str(all) )
     if(verbose) message( tail(all) )
@@ -353,12 +358,11 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
     # color[which(all[,'fld']=="PASS")] <- "#10222222" # grey
     # str(all)
 
-    
         
     if(nr_all>1) { # TODO consider removing !!! due to added boundaries (min/max)
     col_qty <- length(xlab.str)
     chart_all <- parallelplot(~all[1:col_qty], horizontal.axis=FALSE, col=all[,'color'],
-                              main="Argument Combinations vs Test Results (PASS - grey, FAIL - color)"),
+                              main="Argument Combinations vs Test Results (PASS - grey, FAIL - color)",
                               drop.unused.levels=FALSE) # lvls of factors
     # chart_all <- parallelplot(all, horizontal.axis=FALSE, col=rainbow(10))
     print(chart_all)
@@ -372,5 +376,31 @@ plot.tests <- function(env=cont.env, pass=TRUE, fail=TRUE,
 }
 
 
+
+# TODO: print everything on the same chart:
+# reference: http://stackoverflow.com/questions/6853204/plotting-multiple-curves-same-graph-and-same-scale
+# par(new = TRUE)
+
+## plotting several classes in various colors
+## Ref: http://www.public.iastate.edu/~maitra/stat501/Rcode/parallelplot.R
+##      see at the bottom !
+
+## also, see argument 'groups'
+#  groups A variable or expression to be
+#  evaluated in data, expected to act as a grouping variable within each
+#  panel, typically used to distinguish different groups by varying graphical
+#  parameters like color and line type. Formally, if groups is specified, then
+#  groups along with subscripts is passed to the panel function,
+#-> good example for the 'group' parameter
+#   http://www.r-bloggers.com/conditioning-and-grouping-with-lattice-graphics/
+#
+#-> how to use groups:
+#    http://science.nature.nps.gov/im/datamgmt/statistics/r/graphics/lattice.cfm
+#    read that page, which includes the following, among other things:
+#    #---------------------------------------------------------------------#
+#    | y ~ x | b, groups=a    separate plots for each value of b of y ~ x, |
+#    | with dfferent colors or symbols for different values of a           |
+#    #---------------------------------------------------------------------#
+#
 
 
