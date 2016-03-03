7bc62b082ec8a3e9cf2844b16922d25dfe8e5a3f
cloudcello <ef256aff@opayq.com>
2016-03-03 23:20:04 +0300

    Adds Logging Function Draft

--- R/utils.R
+++ R/utils.R
@@ -6,9 +6,62 @@
 #' @export
 waitForUserInput <- function() 
 { 
-    message("Press Enter when ready")
+    message(" ")
+    message("-= Press 'Enter' when ready =-")
+    message(" ")
     invisible(readline()) 
 }
 
 
+#' logged message
+#' @param x value to be submitted as a message to message()
+#' @param logger logger handle which must be set up prior to running 
+#'        lmessage
+#' @param env container-environment of the testing framework
+#' @param verbose (default==TRUE) whether to call message
+#' 
+#' @export
+lmessage <- function(x, logger, env=cont.env, verbose=TRUE)
+{
+    if(!exists(logger)) stop(paste0("trying to write to logger '", logger, 
+                                    "', which has not been set up / 
+                                    initialized yet.")
+    if(verbose) message(x)
+    zz_logger <- env$loggers[lgr_name]
+    
+    cat(x, file = zz_logger, sep = "\n")
+    
+}
 
+#' logger setup
+#' 
+#' @param lgr_name logger name
+#' @param dest destination directory (defaults to the current work directory)
+#' @param env work (container) environment (default 'cont.env')
+#' 
+#' @export
+logger_new <- function(lgr_name, dest=getwd(), fname=NULL, env=cont.env)
+{
+    if(is.null(fname)) {stop("log file name must be provided!")}
+    full_path_and_fname <- paste0(dest,"/",fname)
+    
+    if(!exists(envir = env, "loggers")) {
+        env$loggers <- list()
+    }
+    
+    # TODO: check whether a logger with the same name already exists
+    env$loggers[lgr_name] <- file(full_path_and_fname, "w") 
+    
+}
+
+#' removes a specified logger after closing the log file
+#' 
+#' @param lgr_name logger name
+#' @param env work (container) environment (default 'cont.env')
+#' 
+#' @export
+rm_logger <- function(lgr_name, env=cont.env)
+{
+    # TODO: first, check a logger exists
+    close( env$loggers[lgr_name] )
+}
