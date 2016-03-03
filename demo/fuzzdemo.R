6db15ed739e6d7ba22201d92878bca85bc4b931c
cloudcello <ef256aff@opayq.com>
2016-03-03 03:38:39 +0300

    Completes the First Draft of the Demo

--- demo/fuzzdemo.R
+++ demo/fuzzdemo.R
@@ -8,6 +8,10 @@
 # options: a,b,c
 # suboptions: a,b,c,d
 
+message("Demo of the fuzztest package") 
+
+waitForUserInput()
+
 ################################################################################
 # CONTROL FLOW TEST
 ################################################################################
@@ -22,17 +26,19 @@ if(1) {
     generate.argset(arg_register = r, display_progress=TRUE)
     apply.argset(FUN="fuzzdemofunc")
     test_summary()
-    plot.tests()
+    plot_tests()
     # plot.tests(pass = F)
     # plot.tests(fail = F)
 }
+
+waitForUserInput()
 
 message("The summary shows that the argument 'option' explains the most ") 
 message("variability in the outcome. So let's concentrate on that.")
 message("If we go to the detailed test result table, we will see that most")
 message("failures occur when option value #3 is selected. If we read the log ") 
 message("we will see that the types of errors are mixed. But for now, let's ")
-message("think that we fix the ones related to control flow first. ")
+message("think that we decided to fix the bugs related to control flow first. ")
 message("...")
 message("Let's assume we fixed all the control flow related bugs without ")
 message("actually going and changing anything in the code. For that ")
@@ -40,7 +46,7 @@ message("assumption to work, we will choose a combination of options ")
 message("that resulted in the 'PASS' outcome. Such a combination ")
 message("could be, for example, {x=0, y=0, option='a', suboption='a'}.")
 
-# waitForUserInput()
+waitForUserInput()
 
 ################################################################################
 # NUMERIC TEST
@@ -48,11 +54,8 @@ message("could be, for example, {x=0, y=0, option='a', suboption='a'}.")
 message("...")
 message("Now we will concentrate on the numeric part of the test.")
 message("There are two main approaches here:")
-message(" 1. to generate random numbers within some range and let the ")
-message("    test framework make create a set with all possible combinations ")
-message("    of those options")
-message(" 2. create an evenly spaced sequence of values for each (x and y)")
-message("    parameter from lowest to highest and let the framework ")
+message(" 1. create an evenly spaced sequence of values for each (x and y)")
+message("    parameter from lowest to highest and let the argument set generator")
 message("    combine and test these combinations. This second approach has")
 message("    has an advantage in that it aligns numeric values with ")
 message("    the cardinal number of the group associated with an option")
@@ -62,9 +65,15 @@ message("    21 options for argument 'x'. The visualized test results")
 message("    will list those from 'Min' to 'Max'. So finding errors ")
 message("    will be easier as it will be easier to deduce the dependence ")
 message("    of failures on a range of input values from 'x'. ")
+message(" 2. generate random numbers within some range and let the ")
+message("    test framework generate a data set of all possible combinations")
+message("    of those options.")
+message("...")
+message("Now the first approach will be demonstrated:")
 
+waitForUserInput()
 
-if(0) { 
+if(1) { 
     set.seed(0)
     r <- list()
     r$x <- c(seq(from=-10, to=10, length.out = 21))
@@ -75,13 +84,80 @@ if(0) {
     generate.argset(arg_register = r, display_progress=TRUE)
     apply.argset(FUN="fuzzdemofunc")
     test_summary()
-    plot.tests()
+    plot_tests()
     # plot.tests(pass = F)
     # plot.tests(fail = F)
 }
 
+waitForUserInput()
+
 message("    Now one can clearly see two linear relationships between ")
 message("    'x' and 'y'. These correspond to 'numeric bugs' #1NC and 4NC")
+message("    'x' and 'y'. These correspond to 'numeric bugs' #1NC and 4NC")
+message("...")
+message("Let's assume those are fixed now.")
+
+waitForUserInput()
+
+################################################################################
+# FUZZ TESTING
+################################################################################
+message("...")
+message("Now the second (random testing) approach will be demonstrated")
+message("for numeric arguments only, although character inputs could be ")
+message("easily indexed and randomly selected as well.")
+
+waitForUserInput()
+
+if(1) { 
+    set.seed(0)
+    r <- list()
+    r$x <- runif(15, min=-10, max=10)
+    r$y <- runif(15, min=-10, max=10)
+    r$option <- c("b")
+    r$suboption <- c("a","b","c","d")
+    
+    generate.argset(arg_register = r, display_progress=TRUE)
+    apply.argset(FUN="fuzzdemofunc")
+    test_summary()
+    plot_tests()
+}
+
+waitForUserInput()
+message("Immediately one can see the following:")
+message("The text output shows that suboption #3 ('c') is always failing.")
+message("Those axes that have only one option ('level') should not be plotted")
+message("or placed at the left(/right)most side of the parallel plot. This")
+message("reordering can simply be accomplished by creating a test register")
+message("in a different sequence (or tweaking the internals of the ")
+message("plotting function of this package). Let's simply reorder and use")
+message("a smaller dataset to speed up testing.")
+waitForUserInput()
+
+
+if(1) { 
+    set.seed(0)
+    r <- list()
+    r$x <- runif(5, min=-10, max=10)
+    r$y <- runif(5, min=-10, max=10)
+    r$suboption <- c("a","b","c","d")
+    r$option <- c("b")
+    
+    generate.argset(arg_register = r, display_progress=TRUE)
+    apply.argset(FUN="fuzzdemofunc")
+    test_summary()
+    plot_tests()
+}
+
+waitForUserInput()
+message("Now it's time to eliminate each bug one by one.")
+
+
+
+message("")
+message("-------------")
+message(" End of Demo ")
+message("-------------")
 
 
 
